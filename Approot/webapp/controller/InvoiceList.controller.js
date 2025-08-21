6398331b7e1d299a3b430234180efd4ef61ea5cb
Mahitha <kanchammahitha789@gmail.com>
2025-08-21 17:37:08 +0530

    Step31-Routing with Parameters

--- Approot/webapp/controller/InvoiceList.controller.js
+++ Approot/webapp/controller/InvoiceList.controller.js
@@ -1,54 +1,21 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], (Controller, JSONModel, Filter, FilterOperator) => {
-	"use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], function (Controller, JSONModel, Filter, FilterOperator) {
+    "use strict";
 
-	return Controller.extend("ui5.walkthrough.controller.InvoiceList", { 
-		onInit() {
-			// Local view model for things like currency
-			const oViewModel = new JSONModel({
-				currency: "EUR"
-			});
-			this.getView().setModel(oViewModel, "view");
-		},
+    return Controller.extend("ui5.walkthrough.controller.InvoiceList", {
 
-		// Called when search is triggered
-		onFilterInvoices(oEvent) {
-			const aFilter = [];
-			const sQuery = oEvent.getParameter("query");
+        onInit: function () {},
 
-			if (sQuery) {
-				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
-			}
+        onPress: function (oEvent) {
+            var oItem = oEvent.getSource();
+            var oRouter = this.getOwnerComponent().getRouter();
+            var sPath = oItem.getBindingContext("invoice").getPath().substring(1); // remove leading "/"
+            oRouter.navTo("detail", { invoicePath: encodeURIComponent(sPath) });
+        }
 
-			// filter binding
-			const oList = this.byId("invoiceList");
-			const oBinding = oList.getBinding("items");
-			oBinding.filter(aFilter);
-		},
-
-		// Formatter for the "Status" field in InvoiceList.view.xml
-		statusText(sStatus) {
-			// You can map the status values to readable texts
-			const oBundle = this.getView().getModel("i18n").getResourceBundle();
-			switch (sStatus) {
-				case "A":
-					return oBundle.getText("invoiceStatusA"); // e.g. "New"
-				case "B":
-					return oBundle.getText("invoiceStatusB"); // e.g. "In Progress"
-				case "C":
-					return oBundle.getText("invoiceStatusC"); // e.g. "Done"
-				default:
-					return sStatus;
-			}
-		},
-		onPress() {
-			const oRouter = this.getOwnerComponent().getRouter();
-			oRouter.navTo("detail");
-		}
-
-	});
+    });
 });
