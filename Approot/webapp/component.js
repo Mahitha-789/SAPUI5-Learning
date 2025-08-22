d9b6762c5a01a47ffaa3031b2ace1dcb3ccac88e
Mahitha <kanchammahitha789@gmail.com>
2025-08-22 11:54:33 +0530

    Updated step35 files

--- Approot/webapp/component.js
+++ Approot/webapp/component.js
@@ -1,39 +1,35 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "sap/ui/model/json/JSONModel"
-], function (UIComponent, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/Device"
+], (UIComponent, JSONModel, Device) => {
     "use strict";
 
     return UIComponent.extend("ui5.walkthrough.Component", {
-
         metadata: {
             interfaces: ["sap.ui.core.IAsyncContentCreation"],
             manifest: "json"
         },
 
-        init: function () {
-            // call parent init
+        init() {
+            // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
 
-            // default data model
-            var oData = {
-                recipient: { name: "World" }
+            // set data model
+            const oData = {
+                recipient: {
+                    name: "World"
+                }
             };
-            var oModel = new JSONModel(oData);
+            const oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // invoice model
-            var oInvoiceData = {
-                Invoices: [
-                    { ProductName: "Laptop", Quantity: 2, ExtendedPrice: 1200, ShipperName: "FedEx", Status: "A" },
-                    { ProductName: "Phone", Quantity: 5, ExtendedPrice: 2500, ShipperName: "DHL", Status: "B" },
-                    { ProductName: "Tablet", Quantity: 3, ExtendedPrice: 900, ShipperName: "UPS", Status: "C" }
-                ]
-            };
-            var oInvoiceModel = new JSONModel(oInvoiceData);
-            this.setModel(oInvoiceModel, "invoice");
+            // set device model
+            const oDeviceModel = new JSONModel(Device);
+            oDeviceModel.setDefaultBindingMode("OneWay");
+            this.setModel(oDeviceModel, "device");
 
-            // initialize router
+            // create the views based on the url/hash
             this.getRouter().initialize();
         }
     });
